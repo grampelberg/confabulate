acff3aca7891b565e70a82becc244a959408cd75
Thomas Rampelberg <pyronicide@saunter.org>
2010-04-26 22:40:47 -0700

    Forgot to add test.js

---
+++ test/test.js
@@ -0,0 +1,29 @@
+/*
+ * Simple functional tests for confabulate.js
+ *
+ * Copyright(c) 2010 BitTorrent Inc.
+ * License: ************
+ *
+ * Date: %date%
+ * Version: %version%
+ *
+ */
+
+
+$(document).ready(function() {
+  var aws_queue = sqs('localhost:8080');
+  $("#send :button").click(function() {
+    var queue = $("#send .queue").val();
+    var msg = $("#send .message").val();
+    aws_queue.send(queue, msg, function(resp) {
+      console.log(resp);
+      $("#result").text(JSON.stringify(resp));
+    });
+  });
+  $("#receive :button").click(function() {
+    var queue = $("#receive .queue").val();
+    aws_queue.recv(queue, function(resp) {
+      $("#result").text(resp);
+    });
+  });
+});
